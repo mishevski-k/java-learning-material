0eb0632d410b51faac3b9c67a07024529e44a203
mishevski-k <89786242+mishevski-k@users.noreply.github.com>
2022-06-13 23:08:16 +0200

    Added a small Expressions program in java

---
+++ Expressions/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+        //  Expressions = operands & operators
+        //  Operands = values, variables, numbers, quantity
+        //  Operators = + - * / %
+
+        int friends = 10;
+
+        friends++;
+
+        System.out.println(friends);
+    }
+}
