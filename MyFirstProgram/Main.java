20d7a283e87cd3f4b9277105e3e9d741d5cef39f
mishevski-k <89786242+mishevski-k@users.noreply.github.com>
2022-06-13 21:47:08 +0200

    Added first program in java used to print out string

---
+++ MyFirstProgram/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args){
+        //This is a comment in java
+
+        //  System.out.print is used for printing statements in the console.
+        //  System.out.println is used for printing statement in a new line.\
+        //  Everything starting with a '\' is called a escape sequance and depending of the character next to it can do different functions
+        //  \n is used for printing a new line when it's placed
+        //  \t is used for adding a Tab on the place where its called
+        //  To print '\' you need to type '\\', bacause the IDE will treat everything with just one '\' as a escape sequance.
+        //  Typing '//' is called a comment and everything typed after that two characters will be ignored by the IDE, it can be used to add valuable information to your cooleage, explain the code, and much more
+
+        System.out.print("I love pizaa\n");
+        System.out.println("It's really good!");
+    }
+}
