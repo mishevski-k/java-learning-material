c38164bf67847fa6f3d566a9eddb40a23ba9b94e
mishevski-k <89786242+mishevski-k@users.noreply.github.com>
2022-06-16 21:57:30 +0200

    Added simple MathClass program to find max, and min of two numbers, aswell as absolute value and square root

---
+++ MathClass/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        double x = 3.14;
+        double y = -10;
+
+        double z = Math.max(x, y);
+        System.out.println(Math.max(x, y));
+        System.out.println(Math.min(x, y));
+        System.out.println(Math.abs(y));
+        System.out.println(Math.sqrt(x));
+    }
+}
