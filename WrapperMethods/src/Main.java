c0756a21bec7db878113e8211afef4b53e1a07ed
mishevski-k <89786242+mishevski-k@users.noreply.github.com>
2022-06-19 20:42:03 +0200

    Added program for testing wrrapper methods

---
+++ WrapperMethods/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Boolean _bollean = true;
+        Character _char = '@';
+        Integer _int = 123;
+        Double _double = 3.14;
+        String _string = "Kiril";
+
+        System.out.println(_int.floatValue());
+    }
+}
